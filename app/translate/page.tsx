6c6c900581b819903aa8beceb2f18eaeff28011c
Martin Alejandro Mammana <martin.ale590@gmail.com>
2024-05-06 17:26:55 -0300

    feature textarea & lang select

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,15 +1,26 @@
 import TranslationForm from "@/components/TranslationForm"
+import { TranslationLanguages } from "@/types"
 import { auth } from "@clerk/nextjs/server"
 
-const translatePage = () => {
+const translatePage = async () => {
 
   const { userId } = auth()
   if(!userId) throw new Error("User not logged in")
 
+  const languagesEndpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0"
+
+  const response = await fetch(languagesEndpoint, {
+    next: {
+      revalidate: 60 * 60 * 24
+    }
+  })
+
+  const languages = await response.json() as TranslationLanguages
+
   return (
     <div>
       {/* translateionForm */}
-        <TranslationForm />
+        <TranslationForm languages={languages} />
       {/* translation history */}
     </div>
   )
--- components/TranslationForm.tsx
+++ components/TranslationForm.tsx
@@ -1,8 +1,105 @@
-'use client'
-const TranslationForm = () => {
-  return (
-    <div>TranslationForm</div>
-  )
-}
+"use client";
 
-export default TranslationForm
+import { TranslationLanguages } from "@/types";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+const TranslationForm = ({
+  languages,
+}: {
+  languages: TranslationLanguages;
+}) => {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+
+  return (
+    <div>
+      <form>
+        <div>
+          <Select name='inputLanguage' defaultValue='auto'>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Select a language" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Want us to figure it out?</SelectLabel>
+
+                <SelectItem key="auto" value="auto">
+                  Auto-Detection
+                </SelectItem>
+              </SelectGroup>
+
+              <SelectGroup>
+                <SelectLabel>Langugages</SelectLabel>
+                {Object.entries(languages.translation).map(([key, value]) => (
+                  <SelectItem key={key} value={key}>
+                    {value.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Textarea
+            placeholder="Type your message here..."
+            className="min-h-32 text-xl"
+            name="input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <Select name='outputLanguage' defaultValue='es'>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Select a language" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Want us to figure it out?</SelectLabel>
+
+                <SelectItem key="auto" value="auto">
+                  Auto-Detection
+                </SelectItem>
+              </SelectGroup>
+
+              <SelectGroup>
+                <SelectLabel>Langugages</SelectLabel>
+                {Object.entries(languages.translation).map(([key, value]) => (
+                  <SelectItem key={key} value={key}>
+                    {value.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Textarea
+            placeholder="Type your message here..."
+            className="min-h-32 text-xl"
+            name="output"
+            value={output}
+            onChange={(e) => setOutput(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TranslationForm;
